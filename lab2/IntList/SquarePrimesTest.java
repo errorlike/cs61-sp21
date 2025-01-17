13c771da19cba926d30d2093175468a6b3a56440
errorlike <errorlikeman@gmail.com>
2025-01-17 20:39:23 +0800

    lab2 Debugging

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,25 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquareOnlyOnePrime(){
+        IntList lst = IntList.of(2);
+       boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testHasNumberOne(){
+        IntList lst = IntList.of(1,2,3);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 3", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testRepeatPrimeList(){
+        IntList lst = IntList.of(0,4,8,2,2);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 4 -> 8 -> 4 -> 4", lst.toString());
+        assertTrue(changed);
+    }
 }
