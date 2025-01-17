cfd11ce542b47c522040b2e4112a4ad6c51fbaa0
errorlike <errorlikeman@gmail.com>
2025-01-17 21:29:58 +0800

    lab2 处理head为null的情况

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,11 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+
+    @Test
+    public void testHeadNull() {
+        IntList lst = null;
+        IntListExercises.addConstant(lst, 2);
+        assertNull(lst);
+    }
 }
