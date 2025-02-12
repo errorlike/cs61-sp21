46fdb5dc66206ba51bb20932070e5303130b49a6
errorlike <errorlikeman@gmail.com>
2025-02-12 14:34:58 +0800

    lab3 Adding More Randomized Calls

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,53 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> expected = new AListNoResizing<>();
+        BuggyAList<Integer> actual = new BuggyAList<>();
+
+        expected.addLast(1);
+        expected.addLast(2);
+        expected.addLast(3);
+
+        actual.addLast(1);
+        actual.addLast(2);
+        actual.addLast(3);
+
+        assertEquals(expected.removeLast(), actual.removeLast());
+        assertEquals(expected.removeLast(), actual.removeLast());
+        assertEquals(expected.removeLast(), actual.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            } else if (operationNumber == 2) {
+                if (L.size() == 0) {
+                    continue;
+                }
+                L.getLast();
+            } else if (operationNumber == 3) {
+                if (L.size() == 0) {
+                    continue;
+                }
+                L.removeLast();
+            }
+
+        }
+    }
 }
